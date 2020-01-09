d68119bc2edc718abe0f979d27e23a99d63f2c83
James Corbould <james.corbould@cin7.com>
2020-01-09 21:04:51 +1300

    Wrote some unit tests.

--- PrimeFactorsTest/UnitTest1.cs
+++ PrimeFactorsTest/UnitTest1.cs
@@ -1,14 +1,35 @@
-using System;
 using Xunit;
+using PrimeFactorsLib;
+using System.Collections.Generic;
 
 namespace PrimeFactorsTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test10()
         {
+            var factors = PrimeFactorsHelper.PrimeFactors(10);
+            Assert.True(Product(factors) == 10);
+        }
 
+        [Fact]
+        public void Test1000()
+        {
+            var factors = PrimeFactorsHelper.PrimeFactors(1000);
+            Assert.True(Product(factors) == 1000);
+        }
+
+        public int Product(List<int> primes)
+        {
+            int product = 1;
+
+            foreach(int prime in primes)
+            {
+                product *= prime;
+            }
+
+            return product;
         }
     }
 }
